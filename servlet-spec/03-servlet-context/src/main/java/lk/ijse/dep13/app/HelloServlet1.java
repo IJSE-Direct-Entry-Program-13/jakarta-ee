8b8cf79846879f1276c91f324b8ea2a21e629ef5
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-02-26 13:20:03 +0530

    Add init params to context, filters, servlets

--- servlet-spec/03-servlet-context/src/main/java/lk/ijse/dep13/app/HelloServlet1.java
+++ servlet-spec/03-servlet-context/src/main/java/lk/ijse/dep13/app/HelloServlet1.java
@@ -1,9 +1,14 @@
 package lk.ijse.dep13.app;
 
+import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
-@WebServlet(loadOnStartup = 0)
+@WebServlet(loadOnStartup = 0,
+        initParams = {
+                @WebInitParam(name = "param1", value = "servlet-value1"),
+                @WebInitParam(name = "param2", value = "servlet-value2")
+        })
 public class HelloServlet1 extends HttpServlet {
     static {
         System.out.println("HelloServlet1 class is being initialized");
--- servlet-spec/03-servlet-context/src/main/java/lk/ijse/dep13/app/filter/HelloFilter1.java
+++ servlet-spec/03-servlet-context/src/main/java/lk/ijse/dep13/app/filter/HelloFilter1.java
@@ -1,12 +1,18 @@
 package lk.ijse.dep13.app.filter;
 
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.http.HttpFilter;
 
-@WebFilter
+@WebFilter(filterName = "HelloFilter",
+        urlPatterns = "/",
+        initParams = {
+                @WebInitParam(name = "param1", value = "filter-value1"),
+                @WebInitParam(name = "param2", value = "filter-value2")
+        })
 public class HelloFilter1 extends HttpFilter {
 
-    static{
+    static {
         System.out.println("HelloFilter1 class is being initialized");
     }
 
