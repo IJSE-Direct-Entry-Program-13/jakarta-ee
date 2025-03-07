d468d3e07678de7d8229319dda205e540606b046
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-03-07 12:56:04 +0530

    Add CustomerServlet

---
+++ servlet-spec/04-servlets-and-filters/src/main/java/lk/ijse/dep13/app/CustomerServlet.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep13.app;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "customer-servlet",
+        urlPatterns = "/customers",
+        loadOnStartup = 0)
+public class CustomerServlet extends HttpServlet {
+
+    static {
+        System.out.println("CustomerServlet class object is being initialized");
+    }
+
+    public CustomerServlet() {
+        System.out.println("CustomerServlet instance is being initialized");
+    }
+
+}
