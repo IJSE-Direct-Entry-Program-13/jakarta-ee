777162a2af5d9e7729f71e084187bf38636e09ce
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-03-08 22:21:47 +0530

    Update CustomerServlet

--- servlet-spec/04-servlets-and-filters/src/main/java/lk/ijse/dep13/app/CustomerServlet.java
+++ servlet-spec/04-servlets-and-filters/src/main/java/lk/ijse/dep13/app/CustomerServlet.java
@@ -1,10 +1,12 @@
 package lk.ijse.dep13.app;
 
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
 @WebServlet(name = "customer-servlet",
-        urlPatterns = "/customers", loadOnStartup = 0)
+        urlPatterns = "/customers")
 public class CustomerServlet extends HttpServlet {
 
     static {
@@ -15,4 +17,21 @@ public class CustomerServlet extends HttpServlet {
         System.out.println("CustomerServlet instance is being initialized");
     }
 
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("init(ServletConfig)");
+        System.out.println("Before: " + getServletConfig());
+        super.init(config);
+        System.out.println("After: " + getServletConfig());
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init()");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
 }
